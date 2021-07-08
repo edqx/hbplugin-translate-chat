850a7432a2fb7ea7c5d2337394652b57d986a7a5
Edward Smale <essmale2005@gmail.com>
2021-07-08 17:34:31 +0100

    Herp derp herp

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { GameDataMessage, RpcMessage, SendChatMessage } from "@skeldjs/protocol"
 
 import {
     ClientLanguage,
+    Connection,
     EventListener,
     HindenburgPlugin,
     Plugin,
@@ -32,12 +33,41 @@ const gameKeywordToLocale = {
     [ClientLanguage.Irish]: "ga"
 };
 
+function generateHerpDerp(length: number) {
+    const randomLen = Math.ceil(Math.random() * length);
+    let str = "";
+    for (let i = 0; i < randomLen; i++) {
+        str += " " + (Math.random() > 0.5 ? "herp" : "derp");
+    }
+
+    return str.trim();
+}
+
 @HindenburgPlugin({
     id: "hbplugin-translate-chat",
     version: "1.0.0",
     order: "none"
 })
 export default class extends Plugin {
+    async sendChatMessageTo(senderNetId: number, message: string, connection: Connection) {
+        await connection.sendPacket(
+            new ReliablePacket(
+                connection.getNextNonce(),
+                [
+                    new GameDataMessage(
+                        connection.room!.code,
+                        [
+                            new RpcMessage(
+                                senderNetId,
+                                new SendChatMessage(message)
+                            )
+                        ]
+                    )
+                ]
+            )
+        );
+    }
+
     @EventListener("player.chat")
     async onSendChat(ev: PlayerSendChatEvent<Room>) {
         const playerConnection = ev.room.connections.get(ev.player.id);
@@ -49,79 +79,59 @@ export default class extends Plugin {
         if (ev.message) {
             ev.message.cancel();
 
-            for (const [ , connection ] of ev.room.connections) {
-                if (connection === playerConnection)
-                    continue;
+            if (this.config.herpderp) {
+                const numHerpDerps = ev.chatMessage.split(" ").length;
+                const herpderp = generateHerpDerp(numHerpDerps);
+                
+                for (const [ , connection ] of ev.room.connections) {
+                    if (connection === playerConnection)
+                        continue;
 
-                const targetLocale = (gameKeywordToLocale as any)[connection.language] || "en";
+                    this.sendChatMessageTo(ev.player.control.netid, herpderp, connection);
+                }
+            } else {
+                for (const [ , connection ] of ev.room.connections) {
+                    if (connection === playerConnection)
+                        continue;
 
-                if (sourceLocale !== targetLocale) {
-                    this.logger.info("Attempting translate from %s->%s.",
-                        sourceLocale, targetLocale);
+                    const targetLocale = (gameKeywordToLocale as any)[connection.language] || "en";
 
-                    const res = await fetch("https://libretranslate.de/translate", {
-                        method: "POST",
-                        body: JSON.stringify({
-                            q: ev.chatMessage,
-                            source: sourceLocale,
-                            target: targetLocale
-                        }),
-                        headers: {
-                            "Content-Type": "application/json"
+                    if (sourceLocale !== targetLocale) {
+                        this.logger.info("Attempting translate from %s->%s.",
+                            sourceLocale, targetLocale);
+
+                        const res = await fetch("https://libretranslate.de/translate", {
+                            method: "POST",
+                            body: JSON.stringify({
+                                q: ev.chatMessage,
+                                source: sourceLocale,
+                                target: targetLocale
+                            }),
+                            headers: {
+                                "Content-Type": "application/json"
+                            }
+                        });
+
+                        if (res.status === 200) {
+                            const json = await res.json();
+                            const translatedText = json.translatedText?.trim();
+
+                            const chatMessage = translatedText && translatedText !== ev.chatMessage
+                                ? ev.chatMessage + " (" + translatedText + ")"
+                                : ev.chatMessage;
+
+                            this.logger.info("Got translation:");
+                            this.logger.info(" - Original: %s", ev.chatMessage);
+                            this.logger.info(" - Translated: %s", translatedText);
+
+                            await this.sendChatMessageTo(ev.player.control.netid, chatMessage, connection);
+                        } else {
+                            this.logger.warn("Failed to get message translate from %s->%s: %s",
+                                sourceLocale, targetLocale, res.status);
                         }
-                    });
-
-                    if (res.status === 200) {
-                        const json = await res.json();
-                        const translatedText = json.translatedText?.trim();
-
-                        const chatMessage = translatedText && translatedText !== ev.chatMessage
-                            ? ev.chatMessage + " (" + translatedText + ")"
-                            : ev.chatMessage;
-
-                        this.logger.info("Got translation:");
-                        this.logger.info(" - Original: %s", ev.chatMessage);
-                        this.logger.info(" - Translated: %s", translatedText);
-
-                        await connection.sendPacket(
-                            new ReliablePacket(
-                                connection.getNextNonce(),
-                                [
-                                    new GameDataMessage(
-                                        ev.room.code,
-                                        [
-                                            new RpcMessage(
-                                                ev.player.control.netid,
-                                                new SendChatMessage(chatMessage)
-                                            )
-                                        ]
-                                    )
-                                ]
-                            )
-                        );
                     } else {
-                        this.logger.warn("Failed to get message translate from %s->%s: %s",
-                            sourceLocale, targetLocale, res.status);
+                        await this.sendChatMessageTo(ev.player.control.netid, ev.chatMessage, connection);
                     }
-                } else {
-                    await connection.sendPacket(
-                        new ReliablePacket(
-                            connection.getNextNonce(),
-                            [
-                                new GameDataMessage(
-                                    ev.room.code,
-                                    [
-                                        new RpcMessage(
-                                            ev.player.control.netid,
-                                            new SendChatMessage(
-                                                ev.chatMessage
-                                            )
-                                        )
-                                    ]
-                                )
-                            ]
-                        )
-                    );
                 }
             }
         }
